e0a71f30f05e278eb985b4bb756ea466fce0a6d4
Phi Ai <130706320+enoten@users.noreply.github.com>
2026-02-08 17:54:06 -0500

    Rename greetings_agent to tool_agent and update description

--- tool_agent/agent.py
+++ tool_agent/agent.py
@@ -1,12 +1,22 @@
 from google.adk.agents import Agent
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset
+from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPConnectionParams
+
+
+mcp_server_url = "http://localhost:8000/mcp"
+
+mcp_toolset = MCPToolset(
+    connection_params=StreamableHTTPConnectionParams(
+        url=mcp_server_url,
+    ),
+)
 
 root_agent = Agent(
-    name="greetings_agent",
-    # https://ai.google.dev/gemini-api/docs/models
+    name="tool_agent",
     model="gemini-2.0-flash",
-    description="Greeting agent",
+    description="Tool agent",
     instruction="""
-    You are a helpful assistant that greets the user. 
-    Ask for the user's name and greet them by name.
+    You are a helpful assistant that can use various tools.
     """,
+    tools=[mcp_toolset]
 )
